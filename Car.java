1c1a189e4c7c5130ab9d1c284b6321cee8ffd1aa
AJ <63066039+ajworks13@users.noreply.github.com>
2022-06-19 23:37:43 -0400

    Update Car.java

--- Car.java
+++ Car.java
@@ -4,6 +4,16 @@ public class Car{
     public String color;
     public int seats;
     
+    // Constructor; Should be the same name as class. 
+    // Constructors dont have a return type or void.
+    Car(String model, String color, int seats){
+        this.model = model; // 'this.' points to the variable added before constructor.
+        this.color = color;
+        this.seats = seats;
+    }
+    
+    
+    
     public void display(){
         System.out.println("Model is " + model);
         System.out.println("Color is " + color);
@@ -11,12 +21,12 @@ public class Car{
 
         
     }
-    
+    /*
     public void intialize(String model_s, String red, int i){
         model = model_s;
         color = red;
         seats = i;
     }
-    
+    */
     
 }

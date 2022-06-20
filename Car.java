0bfe7b53361fc92c47d33063b85e20e5149e67b8
AJ <63066039+ajworks13@users.noreply.github.com>
2022-06-19 23:26:28 -0400

    Update Car.java

--- Car.java
+++ Car.java
@@ -8,6 +8,15 @@ public class Car{
         System.out.println("Model is " + model);
         System.out.println("Color is " + color);
         System.out.println("Seats is " + seats);
+
         
     }
+    
+    public void intialize(String model_s, String red, int i){
+        model = model_s;
+        color = red;
+        seats = i;
+    }
+    
+    
 }
